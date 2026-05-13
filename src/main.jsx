2e5c101ffbbeacbb287c9ded473e9c54e8f7d0a7
NatnaelTaddese <natnaeltaddese3@gmail.com>
2026-05-14 02:48:41 +0800

    reroute home to /

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/info/">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
